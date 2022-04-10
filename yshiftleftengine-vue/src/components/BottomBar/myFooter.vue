5806b807cc56d41bfb0fe5e7284982200ae5eb66
Yan Qiao Yu <yqy1160058763@qq.com>
2022-04-11 00:13:53 +0800

    update

--- yshiftleftengine-vue/src/components/BottomBar/myFooter.vue
+++ yshiftleftengine-vue/src/components/BottomBar/myFooter.vue
@@ -1,7 +1,20 @@
 <template>
   <!-- 这个组件用来存放底部 -->
   <div>
-    <el-link @click="openNewTab('https://beian.miit.gov.cn')" class="IPC">粤ICP备2022026889号</el-link>
+    <el-link
+      :underline="false"
+      target="_blank"
+      href="https://beian.miit.gov.cn"
+      class="IPC"
+    >粤ICP备2022026889号</el-link>
+    <el-divider direction="vertical"></el-divider>
+    <img src="../../assets/备案图标.png" alt />
+    <el-link
+      :underline="false"
+      target="_blank"
+      href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=44030602006714"
+      class="IPC"
+    >粤公网安备 44030602006714号</el-link>
   </div>
 </template>
 
