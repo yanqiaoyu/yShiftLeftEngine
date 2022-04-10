d09e0a604b6df7dbe8a28843adfdc985b3c35c95
Yan Qiao Yu <yqy1160058763@qq.com>
2022-04-11 00:06:07 +0800

    update

---
+++ yshiftleftengine-vue/src/components/BottomBar/myFooter.vue
@@ -0,0 +1,16 @@
+<template>
+  <!-- 这个组件用来存放底部 -->
+  <div>
+    <el-link @click="openNewTab('https://beian.miit.gov.cn')" class="IPC">粤ICP备2022026889号</el-link>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="less" scoped>
+.IPC {
+  color: #999999 !important;
+}
+</style>
